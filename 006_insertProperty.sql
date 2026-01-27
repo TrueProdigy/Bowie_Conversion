0882e9334f01d0d3bcf84d152fed0c5b4cf17bb4
Chris Mulkey <github@chrismulkey.com>
2026-01-27 10:52:41 -0600

    propertyAccount fixes
    Merging in an update to fix some issues in propertyAccount with some value buckets, some cap buckets, and the owner sequence.
    
    This does not resolve all cap issues, as mentioned in line 286!
    
    Once we have a CB fix in place, we'll need to consolidate the original propertyAccount insert and this update into a single statement.

--- 006_insertProperty.sql
+++ 006_insertProperty.sql
@@ -279,6 +279,305 @@ JOIN conversionDB.AppraisalAccount aa
    AND aa.OwnerKey IS NOT NULL
 WHERE p.AuditTaxYear BETWEEN @pYearMin AND @pYearMax;
 
+
+
+
+-- Chris: This isn't the "right" way to do this, but I was writing an update for inplace data, not wanting to disrupt things down stream.  This will work, for now, but really, the update below should be the basis for the propertyAccount insert above.
+-- There are still CB concerns with the following update.  https://trueprodigyteam.slack.com/archives/C097WHAJ4TE/p1769526132382179?thread_ts=1769463429.763769&cid=C097WHAJ4TE
+
+drop temporary table if exists newPropertyAccount;
+create temporary table newPropertyAccount as
+select
+p.pYear,
+    p.pID,
+    p.pVersion,
+    p.pRollCorr,
+    p.propType,
+
+ row_number() over (partition by pYear, pID, pVersion, pRollCorr, ownerID order by ownerPct desc) - 1 as ownerSequence,
+    aa.*,
+
+    @createdBy                          AS createdBy,
+    NOW()                               AS createDt
+from property p
+join lateral (
+  select
+         aa.OwnerKey as ownerID,
+         if(p.propType = 'MN', 100, undividedIntFrac * 100) as ownerPct,
+
+
+         aa.TotalLandVal as ownerLandValue,
+         aa.HomesiteLandVal as ownerLandHSValue,
+         aa.OtherLandVal + aa.UnqualAgTimberLandVal as ownerLandNHSValue,
+         (aa.agLandMarketVal + aa.TimberLandMarketVal + aa.RestrictedUseTimberMarketVal) as ownerSULandMktValue,
+         aa.AgLandProdVal + TimberLandProdVal + RestrictedUseTimberProdVal as ownerSUValue,
+
+  aa.TotalImprovementVal as ownerImprovementValue,
+  aa.HomesiteImprovementVal as ownerImprovementHSValue,
+  aa.OtherImprovementVal ownerImprovementNHSValue,
+
+
+  aa.TotalMarketVal as ownerMarketValue,
+  aa.TotalMarketVal - aa.ProductivityLossVal as ownerAppraisedValue,
+
+
+    if(aa.LimitedAppraisedVal > 0, aa.TotalMarketVal - aa.limitedAppraisedVal, 0) as ownerTaxLimitationValue,
+
+    aa.TotalAppraisedVal as ownerNetAppraisedValue,
+
+
+    ifnull(limitationLastYearHSValue,0) as limitationLastYearHSValue,
+    ifnull(limitationLastYearHSValue,0) * .1 as limitationAllowedIncrease,
+    ifnull(limitationLastYearHSValue,0) + (ifnull(limitationLastYearHSValue,0) * .1) as limitationMaxAllowedIncrease,
+
+
+    /*
+  limitationBaseYear,
+limitationBaseYearOverride,
+limitationBaseYearOverrideReason,
+limitationBaseYearDate,
+limitationLastYearHSValueOverride,
+limitationLastYearHSValueOverrideReason,
+limitationNewValue,
+limitationNewValueOverride,
+limitationNewValueOverrideReason,*/
+
+
+
+    HomesiteNewLandVal + OtherNewLandVal +HomesiteNewImprovementVal + OtherNewImprovementVal +HomesiteNewPersonalVal +OtherNewPersonalVal as ownerNewValue,
+
+    HomesiteNewPersonalVal +OtherNewPersonalVal as ownerNewBppValue,
+
+   (HomesiteNewImprovementVal + OtherNewImprovementVal) as ownerNewImprovementValue,
+   HomesiteNewImprovementVal as ownerNewImprovementHSValue,
+OtherNewImprovementVal as ownerNewImprovementNHSValue,
+(HomesiteNewLandVal + OtherNewLandVal) as ownerNewLandValue,
+HomesiteNewLandVal as ownerNewLandHSValue,
+OtherNewLandVal as ownerNewLandNHSValue,
+
+   ifnull(aa.HomesiteLandVal,0) / (ifnull(aa.HomesiteLandVal,0) + ifnull(aa.HomesiteImprovementVal,0)) as ownerHSLandPct,
+   ifnull(aa.HomesiteImprovementVal,0) / (ifnull(aa.HomesiteLandVal,0) + ifnull(aa.HomesiteImprovementVal,0)) as ownerHSImprovementPct,
+
+#applyPctExemptions,
+#hsGroupPct,
+#hsGroupPctBeforeCap,
+#hsGroupValue,
+#hsGroupValueBeforeCap,
+
+null as taxOfficeRefID,
+null as taxOfficeRefID1,
+# null as taxOfficeUniqueID, -- This is a calculated field
+
+
+
+#createdBy,
+#createDt,
+#updatedBy,
+#updateDt,
+
+
+aa.cID as fromPAccountID,
+
+#ownerOmittedImprovementHSValue,
+#ownerOmittedImprovementNHSValue,
+#reestablishLimitationBase,
+#useCustomStartYearCompressCalc,
+#startYearCompressCalc,
+
+  aa.TimberLandMarketVal as ownerTimberLandMktValue,
+  aa.TimberLandProdVal as ownerTimberValue,
+  aa.TimberLandMarketVal - TimberLandProdVal as ownerTimberExclusionValue,
+
+  aa.AgLandMarketVal as ownerAgLandMktValue,
+  aa.AgLandProdVal as ownerAgValue,
+  aa.AgLandMarketVal - AgLandProdVal as ownerAgExclusionValue,
+
+
+
+  aa.RestrictedUseTimberMarketVal as ownerTimber78LandMktValue,
+  aa.RestrictedUseTimberProdVal as ownerTimber78Value,
+  aa.RestrictedUseTimberMarketVal - RestrictedUseTimberProdVal as ownerTimber78ExclusionValue,
+
+  caps.cbTaxLimitationValue as ownerCBTaxLimitationValue,
+  caps.hsTaxLimitationValue as ownerHSTaxLimitationValue
+
+
+  /*
+  cbLimitationLastYearValue,
+cbLimitationAllowedIncrease,
+cbLimitationMaxAllowedIncrease
+*/
+
+
+
+
+
+#cbLimitationBaseYear,
+#cbLimitationBaseYearOverride,
+#cbLimitationBaseYearOverrideReason,
+#cbLimitationBaseYearDate,
+
+
+
+
+
+#cbLimitationLastYearValueOverride,
+#cbLimitationLastYearValueOverrideReason,
+
+#cbLimitationNewValue,
+#cbLimitationNewValueOverride,
+#cbLimitationNewValueOverrideReason,
+
+#cbRestablishLimitationBase,
+#cbLimitationExcludeFromEvaluation,
+
+
+
+#cbLimitationOverride,
+#cbLimitationOverrideReason,
+#cbLimitationIgnoreQualifyYr,
+
+
+
+from conversionDB.AppraisalAccount aa
+  join lateral (
+  select
+    aa.OtherLandVal + aa.UnqualAgTimberLandVal as landNHSValue,
+    aa.TotalMarketVal - ProductivityLossVal as appraisedValue,
+    aa.AgLandMarketVal - aa.aglandProdVal as agLoss,
+    aa.TimberLandMarketVal - aa.timberLandProdVal as timberLoss,
+    if(LimitedAppraisedVal > 0, aa.TotalMarketVal - aa.limitedAppraisedVal, 0) as limitationValue,
+    LocalGenHomesteadVal + StateGenHomesteadVal +
+        LocalOver65Val + StateOver65Val + LocalOver65SurvivingSpouseVal + StateOver65SurvivingSpouseVal +
+        LocalDPVal + StateDPVal + LocalDisabledSurvivingSpouseVal + StateDisabledSurvivingSpouseVal +
+        DVVal +
+        AbsVal +
+        FreeportVal +
+        GoodsInTransitVal +
+        SolarWindVal +
+        HistVal +
+        Hb1200Val +
+        FTZVal +
+        Min500Val +
+        PollutionControlVal +
+        AbatementVal + SpecAbatementVal +
+        MiscVal + SpecMiscExemptionVal +
+        WaterConVal as legacyExemptionValue,
+    aa.TotalAppraisedVal as netAppraisedValue
+  ) calcs
+
+  join lateral (
+    select
+      calcs.limitationValue - HomesiteCapLossVal as cbTaxLimitationValue,
+      HomesiteCapLossVal as hsTaxLimitationValue
+  ) caps
+join lateral (
+  select
+    case
+      when 'CAD' in (JurisdictionType, JurisdictionCd) then 1
+      when 'CNTY' in (JurisdictionType) then 2
+      else 3
+  end as r
+  ) r
+
+  left join lateral (
+    select
+      prev.HomesiteCapVal as limitationLastYearHSValue
+    from conversionDB.AppraisalAccount prev
+    where prev.PropertyKey = aa.PropertyKey
+    and prev.pyear = aa.pyear - 1
+    and prev.jurisdictionCd = aa.jurisdictionCd
+    limit 1
+
+  ) prev on true
+
+    where aa.PropertyKey = p.pID
+   and aa.pyear      = p.pyear
+   and aa.OwnerKey is not null
+  order by r
+  limit 1
+  ) aa
+#where p.pid = 20
+;
+
+
+
+
+
+
+
+
+#Preview
+select npa.*
+from newPropertyAccount npa
+join propertyAccount pa using (pYear, pID, pVersion, pRollCorr, ownerID)
+;
+
+set @p_skipTrigger = 1;
+
+update newPropertyAccount npa
+join propertyAccount pa using (pYear, pID, pVersion, pRollCorr, ownerID)
+set pa.pYear = npa.pYear,
+pa.pID = npa.pID,
+pa.pVersion = npa.pVersion,
+pa.pRollCorr = npa.pRollCorr,
+# pa.propType = npa.propType,
+pa.ownerID = npa.ownerID,
+pa.ownerPct = npa.ownerPct,
+pa.ownerLandValue = npa.ownerLandValue,
+pa.ownerLandHSValue = npa.ownerLandHSValue,
+pa.ownerLandNHSValue = npa.ownerLandNHSValue,
+pa.ownerSULandMktValue = npa.ownerSULandMktValue,
+pa.ownerSUValue = npa.ownerSUValue,
+pa.ownerImprovementValue = npa.ownerImprovementValue,
+pa.ownerImprovementNHSValue = npa.ownerImprovementNHSValue,
+pa.ownerImprovementHSValue = npa.ownerImprovementHSValue,
+pa.ownerMarketValue = npa.ownerMarketValue,
+pa.ownerAppraisedValue = npa.ownerAppraisedValue,
+pa.ownerTaxLimitationValue = npa.ownerTaxLimitationValue,
+pa.ownerNetAppraisedValue = npa.ownerNetAppraisedValue,
+pa.limitationLastYearHSValue = npa.limitationLastYearHSValue,
+pa.limitationAllowedIncrease = npa.limitationAllowedIncrease,
+pa.limitationMaxAllowedIncrease = npa.limitationMaxAllowedIncrease,
+pa.ownerNewValue = npa.ownerNewValue,
+pa.ownerNewBppValue = npa.ownerNewBppValue,
+pa.ownerNewImprovementValue = npa.ownerNewImprovementValue,
+pa.ownerNewImprovementHSValue = npa.ownerNewImprovementHSValue,
+pa.ownerNewImprovementNHSValue = npa.ownerNewImprovementNHSValue,
+pa.ownerNewLandValue = npa.ownerNewLandValue,
+pa.ownerNewLandHSValue = npa.ownerNewLandHSValue,
+pa.ownerNewLandNHSValue = npa.ownerNewLandNHSValue,
+pa.ownerHSLandPct = npa.ownerHSLandPct,
+pa.ownerHSImprovementPct = npa.ownerHSImprovementPct,
+pa.taxOfficeRefID = npa.taxOfficeRefID,
+pa.taxOfficeRefID1 = npa.taxOfficeRefID1,
+# pa.taxOfficeUniqueID = npa.taxOfficeUniqueID,
+pa.ownerSequence = npa.ownerSequence,
+pa.fromPAccountID = npa.fromPAccountID,
+pa.ownerTimberLandMktValue = npa.ownerTimberLandMktValue,
+pa.ownerTimberValue = npa.ownerTimberValue,
+pa.ownerTimberExclusionValue = npa.ownerTimberExclusionValue,
+pa.ownerAgLandMktValue = npa.ownerAgLandMktValue,
+pa.ownerAgValue = npa.ownerAgValue,
+pa.ownerAgExclusionValue = npa.ownerAgExclusionValue,
+pa.ownerTimber78LandMktValue = npa.ownerTimber78LandMktValue,
+pa.ownerTimber78Value = npa.ownerTimber78Value,
+pa.ownerTimber78ExclusionValue = npa.ownerTimber78ExclusionValue,
+pa.ownerCBTaxLimitationValue = npa.ownerCBTaxLimitationValue,
+pa.ownerHSTaxLimitationValue = npa.ownerHSTaxLimitationValue,
+pa.createdBy = npa.createdBy,
+pa.createDt = npa.createDt
+where pa.pAccountID > 0;
+;
+
+
+
+
+
+
+
+
+
 # prop legal description
 INSERT INTO propertyLegalDescription
 (
